626cdb1e0a8a176fcecc9a8edc5d35816eca7f6a
Corey Smith <CSSmith1997@gmail.com>
2021-02-11 18:57:53 -0700

    Added info for manifest.json file, completed service worker file (pre-test)

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const cacheName = 'offline-cache';
 const cachedFiles = [
 '/',
@@ -13,8 +15,47 @@ const cachedFiles = [
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Installed');
     e.waitUntil(caches.open(cacheName).then((cache) => {
-        console.log('[Service Worker] Caching All: app shell and content!');
-        return cache.addAll(cachedFiles);
+        console.log('All content has been cached successfully.');
+        return caches.addAll(cachedFiles);
     })
     );
+});
+
+self.addEventListener('activate', (e) =>{
+    e.waitUntil(cache.keys().then(keyList => {
+        return Promise.all(keyList.map(key => {
+            if (key !== cacheName && key !== cachedFiles) {
+                console.log("Destroying old cached files", key);
+                return caches.delete(key);
+            }
+        })
+        );
+    })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(caches.open(cacheName).then(cache => {
+            return fetch(e.request).then(res => {
+                if(res.status === 200) {
+                    cache.put(e.request.url, res.clone());
+                }
+
+                return res;
+              })
+              .catch(err => {
+            
+                return cache.match(e.request);
+              });
+          }).catch(err => console.log(err))
+        );
+    
+        return;
+        }
+
+        e.respondWith(caches.match(e.req).then(function(res) {
+            return res || fetch(e.req);
+        })
+        );
 });
